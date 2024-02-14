edae948d7d31c7552115c55422df0188c0244395
Bilal Dollan <bilaldollan199@gmail.com>
2024-02-14 22:16:53 +0300

    obstacle collision

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = 1000;
+canvas.width = 1280;
+canvas.height = 720;
 
 
 c = canvas.getContext('2d');
@@ -12,7 +12,8 @@ class Player{
             x:100,
             y:canvas.height - this.height - 10
         }
-        this.width = 100
+        this.width = 88
+        this.height = 94
         this.velocity = {
             x:0,
             y:10
@@ -64,28 +65,111 @@ class Ground{
     }
 }
 
+class Obstacle{
+    constructor(){
+        this.position ={
+            x: Math.floor(Math.random()* 100+ 100) ,
+            y: canvas.height -80
+        }
+        this.width = 34;
+        this.height = 70;
+        this.velocity = 8;
+    }
+
+    draw1(){
+        let obstacleImage = document.getElementById('obstacle');
+        c.drawImage(obstacleImage, this.position.x +1280, this.position.y);
+
+    }
+
+    draw2(){
+        let obstacleImage = document.getElementById('obstacle');
+        c.drawImage(obstacleImage, this.position.x + 2000, this.position.y);
+
+    }
+
+    draw3(){
+        let obstacleImage = document.getElementById('obstacle');
+        c.drawImage(obstacleImage, this.position.x + 3000, this.position.y);
+
+    }
+
+    update(){
+        this.draw1();
+        this.draw2();
+        this.draw3();
+        this.position.x -= this.velocity;
+    }
+} 
+
 const player = new Player();
 const ground = new Ground();
+const obstacle = new Obstacle();
+
+ground.update();
+player.update();
+obstacle.update();
 
 
+let startGame = false;
 
+const button = document.querySelector('button');
 
+button.addEventListener('click', ()=>{
+    startGame = true;
+    animate();
+    button.remove();
+})
 
 function animate(){
+    if (!startGame) return; 
     requestAnimationFrame(animate);
     c.clearRect(0,0, innerWidth, innerHeight);
     ground.update();
     player.update();
+    obstacle.update();
+    if(obstacle.position.x <= -3000){
+        obstacle.position.x =  Math.floor(Math.random()* 100);
+        obstacle.update();
+        obstacle.update();
+    }
+
     if (ground.position.x <= -2399){
         ground.position.x = 0 
     }
-}
 
-animate();
-addEventListener('keydown', ({code})=>{
+    if ((obstacle.position.x + 1280 + obstacle.width) - player.position.x - player.width   <= 8 
+            && (obstacle.position.x + 1280 + obstacle.width) - player.position.x - player.width   >= -8
+            && player.position.y + player.height > obstacle.position.y){
+        startGame = false
+
+    } else if((obstacle.position.x + 2000 + obstacle.width) - player.position.x - player.width   <= 8
+            && (obstacle.position.x + 2000 + obstacle.width) - player.position.x - player.width   >= -8
+            && player.position.y + player.height > obstacle.position.y){
+        startGame = false
+
+    }else if((obstacle.position.x + 3000 + obstacle.width) - player.position.x - player.width   <= 8 
+            && (obstacle.position.x + 3000 + obstacle.width) - player.position.x - player.width   >= -8
+            && player.position.y + player.height > obstacle.position.y){
+        startGame = false
+    }
+
+
+    console.log(player.position.y)
+    }
+
+
+
+  
+
+
+    addEventListener('keydown', ({code})=>{
     console
     if (code == 'Space' && player.velocity.y == 0){
-        player.velocity.y = -20; 
-        console.log(player.position.y)
-    }    
-})
+        player.velocity.y = -20;
+    }})
+
+
+
+
+
